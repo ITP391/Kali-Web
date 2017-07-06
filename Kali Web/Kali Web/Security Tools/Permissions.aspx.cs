ebed65f75f99519d082886337ea5b8c012452512
Chu Angie <isadlife@gmail.com>
2017-07-06 11:26:35 +0800

    Added some columns for db

---
+++ Kali Web/Kali Web/Security Tools/Permissions.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kali_Web.Security_Tools
+{
+    public partial class StudList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SqlConnection myConnection;
+            using (myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            {
+                myConnection.Open();
+
+                string query = "SELECT * FROM [User] WHERE [Permission] = @Student";
+                SqlCommand myCommand = new SqlCommand(query, myConnection);
+                myCommand.CommandType = CommandType.Text;
+                myCommand.Parameters.AddWithValue("@email", inputemail);
+
+
+                SqlDataReader reader = myCommand.ExecuteReader();
+
+            }
+        }
+    }
+}
--- Kali Web/Kali Web/Security Tools/StudList.aspx
+++
@@ -1,4 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StudList.aspx.cs" Inherits="Kali_Web.Security_Tools.StudList" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <!-- add codes to show list of students from database-->
-</asp:Content>
